76b40ca3bc9c73c484da18eb692d1727ca38a391
HoMaiQue <homaique.it@gmail.com>
2023-02-18 11:44:56 +0700

    init project

---
+++ server.js
@@ -0,0 +1,13 @@
+// init port, network and start
+
+const app = require("./src/app");
+
+const PORT = 3055;
+const server = app.listen(PORT, () => {
+    console.log("WSV commercial start with port " + PORT);
+});
+
+process.on("SIGINT", () =>{
+    server.close(()=> console.log("Exits Server Express"))
+}
+)
