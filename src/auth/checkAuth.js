c9bdd9dbdb8208601862d5ad3651353d13f265a2
HoMaiQue <homaique.it@gmail.com>
2023-03-05 00:02:21 +0700

    check apikey and permissiton

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const { findById } = require("../services/apiKey.service");
+
+const HEADER = {
+    API_KEY: "x-api-key",
+    AUTHORIZATION: "authorization",
+};
+
+const apiKey = async (req, res, next) => {
+    try {
+        const key = req.headers[HEADER.API_KEY]?.toString();
+        if (!key) {
+            return res.status(403).json({ message: "Forbidden Error" });
+        }
+        const objectKey = await findById(key);
+        if (!objectKey) {
+            return res.status(403).json({ message: "Forbidden Error" });
+        }
+        req.objectKey = objectKey;
+        return next();
+    } catch (error) {
+        console.log(error);
+    }
+};
+const permission = (permission) => {
+    return (req, res, next) => {
+        if (!req.objectKey.permissions) {
+            return res.status(403).json({ message: "Permission denied" });
+        }
+
+        const validPermissions = req.objectKey.permissions.includes(permission);
+        if (!validPermissions) {
+            return res.status(403).json({ message: "Permission denied" });
+        }
+        return next();
+    };
+};
+module.exports = { apiKey, permission };
---
+++ src/services/apiKey.service.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const apiKeysModel = require("../models/apiKeys.model");
+const crypto = require("crypto");
+const findById = async (key) => {
+    // const create = await apiKeysModel
+    //     .create({
+    //         key: crypto.randomBytes(64).toString("hex"),
+    //         permissions: ["000"],
+    //     })
+
+    const objectKey = await apiKeysModel.findOne({ key, status: true }).lean();
+    return objectKey;
+};
+module.exports = { findById };
